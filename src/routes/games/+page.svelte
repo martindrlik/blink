50379fbeb6ad1d72303de884d7947d15be53f4d2
Martin Drlík <533091+martindrlik@users.noreply.github.com>
2025-12-21 22:40:38 +0100

    games menu

---
+++ src/routes/games/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+	import { resolve } from '$app/paths';
+</script>
+
+<div class="h-screen flex flex-col items-center justify-center">
+	{#snippet a(route: string, label: string)}
+		<a class="hover:text-pink-600 text-2xl" href={resolve(route)}>{label}</a>
+	{/snippet}
+	{@render a('/games/rock-paper-scissors', 'Hackable Rock Paper Scissors')}
+	{@render a('/games/target', 'Frustrating Target')}
+</div>
